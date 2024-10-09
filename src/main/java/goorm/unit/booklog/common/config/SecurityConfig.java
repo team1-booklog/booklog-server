41790144710b9a506f83cd7e8c60b2f3979e7751
LeeHanEum <LeeHanEum160924@outlook.kr>
2024-10-10 02:07:25 +0900

    [BOOK-14]-refactor: API 호출 권한 검증 시 토큰 인증 필터 추가

--- src/main/java/goorm/unit/booklog/common/config/SecurityConfig.java
+++ src/main/java/goorm/unit/booklog/common/config/SecurityConfig.java
@@ -5,6 +5,9 @@ import java.util.Collections;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -12,9 +15,13 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
+import goorm.unit.booklog.common.auth.application.UserDetailService;
+import goorm.unit.booklog.common.auth.filter.TokenAuthenticationFilter;
+import goorm.unit.booklog.common.auth.jwt.TokenProvider;
 import lombok.RequiredArgsConstructor;
 
 @Configuration
@@ -22,6 +29,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @EnableMethodSecurity(securedEnabled = true)
 public class SecurityConfig {
+	private final TokenProvider tokenProvider;
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -34,6 +42,7 @@ public class SecurityConfig {
 			.sessionManagement(session -> session
 				.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 			)
+			.addFilterBefore(tokenAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
 			.authorizeHttpRequests(auth -> auth
 				.requestMatchers(SWAGGER_PATTERNS).permitAll()
 				.requestMatchers(STATIC_RESOURCES_PATTERNS).permitAll()
@@ -59,8 +68,8 @@ public class SecurityConfig {
 		"/error",
 		"/favicon.ico",
 		"/index.html",
-			"/api/v1/users/signup",
-			"/api/v1/auth/login"
+		"/api/v1/users/signup",
+		"/api/v1/auth/login"
 	};
 
 	CorsConfigurationSource corsConfigurationSource() {
@@ -74,6 +83,21 @@ public class SecurityConfig {
 		};
 	}
 
+	@Bean
+	public TokenAuthenticationFilter tokenAuthenticationFilter() {
+		return new TokenAuthenticationFilter(tokenProvider);
+	}
+
+	@Bean
+	public AuthenticationManager authenticationManager(
+		BCryptPasswordEncoder bCryptPasswordEncoder,
+		UserDetailService userDetailService
+	){
+		DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+		authProvider.setUserDetailsService(userDetailService);
+		authProvider.setPasswordEncoder(bCryptPasswordEncoder);
+		return new ProviderManager(authProvider);
+	}
 
 	@Bean
 	public BCryptPasswordEncoder bCryptPasswordEncoder() {
