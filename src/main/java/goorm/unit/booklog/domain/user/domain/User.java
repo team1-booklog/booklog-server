dbd641c493e4ef74efca52b105d80fff9dcdc528
LeeHanEum <LeeHanEum160924@outlook.kr>
2024-10-10 02:06:39 +0900

    [BOOK-14]-feat: 유저 도서 간 다대다 매핑

--- src/main/java/goorm/unit/booklog/domain/user/domain/User.java
+++ src/main/java/goorm/unit/booklog/domain/user/domain/User.java
@@ -1,9 +1,15 @@
 package goorm.unit.booklog.domain.user.domain;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.List;
 
 import goorm.unit.booklog.common.domain.BaseTimeEntity;
+import goorm.unit.booklog.domain.book.domain.Book;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -21,7 +27,6 @@ import jakarta.persistence.Table;
 @Table(name="\"user\"")
 @AllArgsConstructor
 @NoArgsConstructor(access= AccessLevel.PROTECTED)
-
 public class User extends BaseTimeEntity implements UserDetails {
 
         @Id
@@ -33,6 +38,14 @@ public class User extends BaseTimeEntity implements UserDetails {
 
         @Column(nullable=false)
         private String password;
+
+        @ManyToMany
+        @JoinTable(
+            name = "user_books",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "book_id")
+        )
+        private List<Book> books = new ArrayList<>();
 
         public static User create(String id, String name, String password) {
                 return User.builder()
